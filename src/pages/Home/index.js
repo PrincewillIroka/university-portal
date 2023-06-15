54a3e2e271e8997d0d825d04850e0d9159ceb88c
Princewill Iroka <princewilliroka@outlook.com>
2023-06-15 22:37:48 +0100

    Update Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import { BiSearch } from "react-icons/bi";
-import "./Home.css";
+import { BsArrowRight } from "react-icons/bs";
+import "./home.css";
 import Logo from "../../assets/Logo.svg";
+import UniversityEntrance from "../../assets/UniversityEntrance.svg";
 
 function Home() {
+  const moreProgramsRef = useRef();
+
+  const handleViewMorePrograms = () => {
+    console.log(moreProgramsRef);
+    const offset = moreProgramsRef.current.offsetLeft + 350;
+    moreProgramsRef.current.scroll({
+      left: offset,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <div className="home-container">
       <header className="header">
@@ -24,8 +37,8 @@ function Home() {
         </ul>
         <BiSearch className="search-icon" />
       </header>
-      <section className="body">
-        <div className="top-section">
+      <section>
+        <section className="top-section">
           <div className="top-section-content">
             <div className="university-title">
               <span>University</span>
@@ -51,7 +64,62 @@ function Home() {
               </div>
             </div>
           </div>
-        </div>
+        </section>
+        <section className="section-two">
+          <div className="section-two-img-container">
+            <img src={UniversityEntrance} alt="" className="section-two-img" />
+          </div>
+          <div className="section-two-right-container">
+            <h3 className="welcome-heading">WELCOME TO OUR UNIVERSITY</h3>
+            <span className="welcome-description">
+              We are a reference University in the South region, with a wide
+              structure, qualified professors and courses recognized by the MEC,
+              we provide teaching, research and extension activities in the most
+              diverse areas of knowledge.
+            </span>
+            <button className="welcome-btn">Continue</button>
+          </div>
+        </section>
+        <section className="section-three">
+          <div className="section-three-col-1">
+            <h3 className="know-our-courses-heading">
+              FIND OUT <br />
+              ABOUT OUR <br /> PROGRAMS
+            </h3>
+            <h5 className="know-our-courses-info">
+              A universe of opportunities.
+            </h5>
+            <div
+              className="check-out-container"
+              onClick={handleViewMorePrograms}
+            >
+              <span className="check-out-heading">View more</span>
+              <BsArrowRight className="check-out-icon" />
+            </div>
+          </div>
+          <div className="section-three-col-2" ref={moreProgramsRef}>
+            <div className="view-program-container">
+              <span>Content 1</span>
+            </div>
+            <div className="view-program-container">
+              <span>Content 2</span>
+            </div>
+            <div className="view-program-container">
+              <span>Content 3</span>
+            </div>
+            <div className="view-program-container">
+              <span>Content 4</span>
+            </div>
+          </div>
+        </section>
+        <section className="section-four">
+          <div className="">
+            <h3>WHAT OUR STUDENTS SAY</h3>
+          </div>
+        </section>
+        <footer className="footer">
+          <span>University Portal &copy; 2023</span>
+        </footer>
       </section>
     </div>
   );
