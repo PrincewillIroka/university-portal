e61dcf6641f9f6162c6e7586fb53c72d4937f0e7
Princewill Iroka <princewilliroka@outlook.com>
2023-06-19 23:58:39 +0100

    Added faq to About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./about.css";
 import { Header, Footer, ReceiveInfo } from "../Home/components";
 
+const FAQ_QUESTIONS = [
+  {
+    question: "How can I gain admission into the university ?",
+    answer: `
+  Step 1: Finding the right program Step 2: Application to Faculty
+              Step 3: Receiving the Conditional Acceptance Letter Step 4:
+              Receiving the unconditional Acceptance Letter Step 5: Obtaining
+              Visa Step 6: Accommodation
+  `,
+  },
+  {
+    question: "When can I apply for  program ?",
+    answer:
+      "You can apply throughout the year for any program of your choice, we'll do well to reach out to you for proper follow-up",
+  },
+  {
+    question: "What are the requirements for an MSc program ?",
+    answer:
+      "The first requirement is a BSc in course related to the one you want to study. Then you also need to have your transcript and other relevant documents ready for whenever we ask for them.",
+  },
+];
+
 function About() {
+  const [activeAnswer, setActiveAnswer] = useState();
+
+  const handleSetAnswer = (value) => {
+    const newActiveAnswer = activeAnswer === value ? "" : value;
+    setActiveAnswer(newActiveAnswer);
+  };
+
   return (
     <div className="about-container">
       <Header />
@@ -147,6 +176,29 @@ function About() {
             />
           </div>
         </div>
+      </section>
+      <section className="about-section-five">
+        <h2 className="faq-heading">Frequently Asked Questions</h2>
+        {FAQ_QUESTIONS.map((faq) => (
+          <div className="faq-single">
+            <div className="faq-row">
+              <span className="faq-question">{faq.question}</span>
+              <span
+                className="faq-plus-icon"
+                onClick={() => handleSetAnswer(faq.question)}
+              >
+                &#43;
+              </span>
+            </div>
+            <span
+              className={`faq-answer ${
+                activeAnswer === faq.question && "faq-answer-active"
+              }`}
+            >
+              {faq.answer}
+            </span>
+          </div>
+        ))}
       </section>
       <ReceiveInfo />
       <Footer />
