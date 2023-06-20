3f126d81eba6c2ae09413e96e1a0be15be5452c1
Princewill Iroka <princewilliroka@outlook.com>
2023-06-20 22:38:57 +0100

    Added active link to Header

--- src/pages/Home/components/Header/index.js
+++ src/pages/Home/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { BiSearch } from "react-icons/bi";
 import "./header.css";
 
 function Header() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const getActiveTab = (value) => location?.pathname?.includes(value);
 
   function handleClick() {
     navigate("/login");
@@ -14,16 +17,24 @@ function Header() {
     <header className="header">
       <img src="/images/logo.svg" className="logo" alt="Logo" />
       <ul className="nav-links-container">
-        <li className="nav-link">
+        <li className={`nav-link ${getActiveTab("home") && "active-nav-link"}`}>
           <a href="/home">Home</a>
         </li>
-        <li className="nav-link">
+        <li
+          className={`nav-link ${getActiveTab("about") && "active-nav-link"}`}
+        >
           <a href="/about">About</a>
         </li>
-        <li className="nav-link">
+        <li
+          className={`nav-link ${
+            getActiveTab("programs") && "active-nav-link"
+          }`}
+        >
           <a href="/programs">Programs</a>
         </li>
-        <li className="nav-link">
+        <li
+          className={`nav-link ${getActiveTab("contact") && "active-nav-link"}`}
+        >
           <a href="/contact">Contact</a>
         </li>
       </ul>
