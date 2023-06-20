45eb72185e32d83b85bf7cdab1608c1c7975fd35
Princewill Iroka <princewilliroka@outlook.com>
2023-06-20 22:37:56 +0100

    Updated Contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import "./contact.css";
+import { Header, Footer, ReceiveInfo } from "../Home/components";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-container">
+      <Header />
+      <section className="contact-top-section">
+        <div className="contact-top-section-content">
+          <span className="contact-best-for-you-text">
+            Reach out to us, to make an inquiry
+          </span>
+        </div>
+      </section>
+      <section className="contact-section-two">
+        <div className="contact-section-two-content">
+          <div className="contact-col-1">
+            <h2 className="contact-heading">Contact Us</h2>
+            <input placeholder="Name" className="contact-input-field" />
+            <input placeholder="Email" className="contact-input-field" />
+            <textarea className="contact-textarea"></textarea>
+            <button className="contact-btn">Send</button>
+          </div>
+          <div className="contact-col-2">
+            <address>123 New Drive Layout, Good Road, One Province</address>
+            <span>campus@universityportal.com</span>
+            <span>(41) 23456789</span>
+            <div className="contact-map"></div>
+          </div>
+        </div>
+      </section>
+      <ReceiveInfo />
+      <Footer />
+    </div>
+  );
 }
 
 export default Contact;
