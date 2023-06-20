6c229382974c3cf0b9ead3e32a6b2c636b5ccf90
Princewill Iroka <princewilliroka@outlook.com>
2023-06-20 20:50:29 +0100

    Update

---
+++ src/data/index.js
@@ -0,0 +1,21 @@
+export const FAQ_QUESTIONS = [
+  {
+    question: "How can I gain admission into the university ?",
+    answer: `
+    Step 1: Finding the right program Step 2: Application to Faculty
+                Step 3: Receiving the Conditional Acceptance Letter Step 4:
+                Receiving the unconditional Acceptance Letter Step 5: Obtaining
+                Visa Step 6: Accommodation
+    `,
+  },
+  {
+    question: "When can I apply for  program ?",
+    answer:
+      "You can apply throughout the year for any program of your choice, we'll do well to reach out to you for proper follow-up",
+  },
+  {
+    question: "What are the requirements for an MSc program ?",
+    answer:
+      "The first requirement is a BSc in course related to the one you want to study. Then you also need to have your transcript and other relevant documents ready for whenever we ask for them.",
+  },
+];
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,28 +1,7 @@
 import React, { useState } from "react";
 import "./about.css";
 import { Header, Footer, ReceiveInfo } from "../Home/components";
-
-const FAQ_QUESTIONS = [
-  {
-    question: "How can I gain admission into the university ?",
-    answer: `
-  Step 1: Finding the right program Step 2: Application to Faculty
-              Step 3: Receiving the Conditional Acceptance Letter Step 4:
-              Receiving the unconditional Acceptance Letter Step 5: Obtaining
-              Visa Step 6: Accommodation
-  `,
-  },
-  {
-    question: "When can I apply for  program ?",
-    answer:
-      "You can apply throughout the year for any program of your choice, we'll do well to reach out to you for proper follow-up",
-  },
-  {
-    question: "What are the requirements for an MSc program ?",
-    answer:
-      "The first requirement is a BSc in course related to the one you want to study. Then you also need to have your transcript and other relevant documents ready for whenever we ask for them.",
-  },
-];
+import { FAQ_QUESTIONS } from "../../data";
 
 function About() {
   const [activeAnswer, setActiveAnswer] = useState();
