fdcea0f09a0c25ba63e96434b345cf4eb9639124
Princewill Iroka <princewilliroka@outlook.com>
2023-07-02 19:16:37 +0100

    Added animation to Home page sections onScroll

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,6 +8,9 @@ import { WHAT_OUR_STUDENTS_SAY } from "../../data";
 
 function Home() {
   const moreProgramsRef = useRef();
+  const sectionTwoRef = useRef();
+  const sectionThreeRef = useRef();
+  const sectionFourRef = useRef();
   const { state } = useStateValue();
   const navigate = useNavigate();
   const { isHeaderModalVisible = false } = state || {};
@@ -35,11 +38,47 @@ function Home() {
     navigate("/programs");
   };
 
+  const handleScroll = (e) => {
+    e.preventDefault();
+    const sectionTwoElement = sectionTwoRef.current;
+    const sectionThreeElement = sectionThreeRef.current;
+    const sectionFourElement = sectionFourRef.current;
+
+    const value1 = isInViewport(sectionTwoElement);
+    const value2 = isInViewport(sectionThreeElement);
+    const value3 = isInViewport(sectionFourElement);
+
+    if (value1) {
+      sectionTwoElement.classList.add("section-two-wrapper-animation");
+    }
+    if (value2) {
+      sectionThreeElement.classList.add("section-three-wrapper-animation");
+    }
+    if (value3) {
+      sectionFourElement.classList.add("section-four-wrapper-animation");
+    }
+
+    //Todo: Detect current scroll position and use it to add the new classes
+  };
+
+  function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    console.log({ rect });
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
   return (
     <div
       className={`home-container container-scroll-enabled ${
         isHeaderModalVisible && "container-scroll-disabled"
       }`}
+      onScroll={handleScroll}
     >
       <Header />
       <section className="home-top-section">
@@ -72,140 +111,146 @@ function Home() {
         </div>
       </section>
       <section className="section-two">
-        <div className="section-two-img-container">
-          <img
-            src="/images/university-entrance.svg"
-            alt=""
-            className="section-two-img"
-          />
-        </div>
-        <div className="section-two-right-container">
-          <h3 className="welcome-heading">WELCOME TO OUR UNIVERSITY</h3>
-          <span className="welcome-description">
-            We are a reference University in the South region, with a wide
-            structure, qualified professors and courses recognized by the MEC,
-            we provide teaching, research and extension activities in the most
-            diverse areas of knowledge.
-          </span>
-          <button className="welcome-btn">Continue</button>
+        <div className="section-two-wrapper" ref={sectionTwoRef}>
+          <div className="section-two-img-container">
+            <img
+              src="/images/university-entrance.svg"
+              alt=""
+              className="section-two-img"
+            />
+          </div>
+          <div className="section-two-right-container">
+            <h3 className="welcome-heading">WELCOME TO OUR UNIVERSITY</h3>
+            <span className="welcome-description">
+              We are a reference University in the South region, with a wide
+              structure, qualified professors and courses recognized by the MEC,
+              we provide teaching, research and extension activities in the most
+              diverse areas of knowledge.
+            </span>
+            <button className="welcome-btn">Continue</button>
+          </div>
         </div>
       </section>
       <section className="section-three">
-        <div className="section-three-col-1">
-          <h3 className="know-our-courses-heading">
-            FIND OUT <br />
-            ABOUT OUR <br /> FACULTIES
-          </h3>
-          <h5 className="know-our-courses-info">
-            A universe of opportunities.
-          </h5>
-          <div
-            className="check-out-container"
-            onClick={handleViewMoreFaculties}
-          >
-            <span className="check-out-heading">View more</span>
-            <BsArrowRight className="check-out-icon" />
-          </div>
-        </div>
-        <div className="section-three-col-2" ref={moreProgramsRef}>
-          <div className="view-faculties-container">
-            <img
-              src="/images/chula-faculty-education-hero-desktop.jpeg"
-              alt="Education faculty"
-              className="view-faculties-img"
-            />
-            <div className="view-faculties-overlay">
-              <span className="faculty-title">Education</span>
+        <div className="section-three-wrapper" ref={sectionThreeRef}>
+          <div className="section-three-col-1">
+            <h3 className="know-our-courses-heading">
+              FIND OUT <br />
+              ABOUT OUR <br /> FACULTIES
+            </h3>
+            <h5 className="know-our-courses-info">
+              A universe of opportunities.
+            </h5>
+            <div
+              className="check-out-container"
+              onClick={handleViewMoreFaculties}
+            >
+              <span className="check-out-heading">View more</span>
+              <BsArrowRight className="check-out-icon" />
             </div>
           </div>
-          <div className="view-faculties-container">
-            <img
-              src="/images/sc-lab.jpeg"
-              alt="Science/Technology faculty"
-              className="view-faculties-img"
-            />
-            <div className="view-faculties-overlay">
-              <span className="faculty-title">Science/Technology</span>
+          <div className="section-three-col-2" ref={moreProgramsRef}>
+            <div className="view-faculties-container">
+              <img
+                src="/images/chula-faculty-education-hero-desktop.jpeg"
+                alt="Education faculty"
+                className="view-faculties-img"
+              />
+              <div className="view-faculties-overlay">
+                <span className="faculty-title">Education</span>
+              </div>
             </div>
-          </div>
-          <div className="view-faculties-container">
-            <img
-              src="/images/cee-pgt.jpeg"
-              alt="Engineering faculty"
-              className="view-faculties-img"
-            />
-            <div className="view-faculties-overlay">
-              <span className="faculty-title">Engineering</span>
+            <div className="view-faculties-container">
+              <img
+                src="/images/sc-lab.jpeg"
+                alt="Science/Technology faculty"
+                className="view-faculties-img"
+              />
+              <div className="view-faculties-overlay">
+                <span className="faculty-title">Science/Technology</span>
+              </div>
             </div>
-          </div>
-          <div className="view-faculties-container">
-            <img
-              src="/images/arts-activities.jpeg"
-              alt="Arts faculty"
-              className="view-faculties-img"
-            />
-            <div className="view-faculties-overlay">
-              <span className="faculty-title">Arts</span>
+            <div className="view-faculties-container">
+              <img
+                src="/images/cee-pgt.jpeg"
+                alt="Engineering faculty"
+                className="view-faculties-img"
+              />
+              <div className="view-faculties-overlay">
+                <span className="faculty-title">Engineering</span>
+              </div>
+            </div>
+            <div className="view-faculties-container">
+              <img
+                src="/images/arts-activities.jpeg"
+                alt="Arts faculty"
+                className="view-faculties-img"
+              />
+              <div className="view-faculties-overlay">
+                <span className="faculty-title">Arts</span>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section className="section-four">
-        <div className="what-students-say-col-1">
-          <h3 className="what-students-say-heading">WHAT OUR STUDENTS SAY</h3>
-          {WHAT_OUR_STUDENTS_SAY.slice(0, 1).map((obj, index) => (
-            <div className="what-students-say-content" key={index}>
-              <div className="what-students-say-content-row">
-                <img
-                  src={`/images/what-our-students-say/${obj.img}`}
-                  alt="Student"
-                  className="what-students-say-photo"
-                />
-                <div className="what-students-say-profile">
-                  <span className="what-students-say-name">{obj.name}</span>
-                  <span className="what-students-say-graduated">
-                    {obj.graduated}
-                  </span>
+        <div className="section-four-wrapper" ref={sectionFourRef}>
+          <div className="what-students-say-col-1">
+            <h3 className="what-students-say-heading">WHAT OUR STUDENTS SAY</h3>
+            {WHAT_OUR_STUDENTS_SAY.slice(0, 1).map((obj, index) => (
+              <div className="what-students-say-content" key={index}>
+                <div className="what-students-say-content-row">
+                  <img
+                    src={`/images/what-our-students-say/${obj.img}`}
+                    alt="Student"
+                    className="what-students-say-photo"
+                  />
+                  <div className="what-students-say-profile">
+                    <span className="what-students-say-name">{obj.name}</span>
+                    <span className="what-students-say-graduated">
+                      {obj.graduated}
+                    </span>
+                  </div>
                 </div>
+                <span className="what-students-say-description">
+                  {obj.description}
+                </span>
               </div>
-              <span className="what-students-say-description">
-                {obj.description}
-              </span>
+            ))}
+            <div className="what-students-say-nav-row">
+              <BsArrowLeft className="what-students-say-arrows" />
+              <BsArrowRight className="what-students-say-arrows what-students-say-arrows-is-allowed" />
             </div>
-          ))}
-          <div className="what-students-say-nav-row">
-            <BsArrowLeft className="what-students-say-arrows" />
-            <BsArrowRight className="what-students-say-arrows what-students-say-arrows-is-allowed" />
           </div>
-        </div>
-        <div className="what-students-say-col-2">
-          <img
-            src="/images/what-our-students-say/medium-shot-student-holding-books.jpg"
-            alt="Student"
-            className="what-students-say-hero-img1"
-          />
-          <div className="what-students-say-detailed-col">
-            <div className="where-our-students-work-container">
-              <h4 className="what-students-say-detailed-heading">
-                Where our students work
-              </h4>
-              <span className="what-students-say-detailed-info">
-                Our students work in the best institutions all over the world.
-                Top global firms like JP Morgan, Google, KPMG, Facebook, Apple,
-                Tesla, Arlbemade e.t.c have alumni who finished from our
-                university.
-              </span>
-            </div>
-            <div className="entrepreneurship-container">
-              <h4 className="what-students-say-detailed-heading">
-                Entrepreneurial Studies
-              </h4>
-              <span className="what-students-say-detailed-info">
-                We have a great pedigree in teaching entrepreneurial studies to
-                students in our institution. From the first semester up till the
-                last, we inculcate valuable tutorials to enable them thrive in
-                starting businesses after they graduate.
-              </span>
+          <div className="what-students-say-col-2">
+            <img
+              src="/images/what-our-students-say/medium-shot-student-holding-books.jpg"
+              alt="Student"
+              className="what-students-say-hero-img1"
+            />
+            <div className="what-students-say-detailed-col">
+              <div className="where-our-students-work-container">
+                <h4 className="what-students-say-detailed-heading">
+                  Where our students work
+                </h4>
+                <span className="what-students-say-detailed-info">
+                  Our students work in the best institutions all over the world.
+                  Top global firms like JP Morgan, Google, KPMG, Facebook,
+                  Apple, Tesla, Arlbemade e.t.c have alumni who finished from
+                  our university.
+                </span>
+              </div>
+              <div className="entrepreneurship-container">
+                <h4 className="what-students-say-detailed-heading">
+                  Entrepreneurial Studies
+                </h4>
+                <span className="what-students-say-detailed-info">
+                  We have a great pedigree in teaching entrepreneurial studies
+                  to students in our institution. From the first semester up
+                  till the last, we inculcate valuable tutorials to enable them
+                  thrive in starting businesses after they graduate.
+                </span>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Program/index.js
+++ src/pages/Program/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Header, Footer, ReceiveInfo } from "../Home/components";
 
 function Program() {
-  return <div>Each Program here</div>;
+  return (
+    <div>
+      <Header />
+      <section>
+        <div>
+          <div>Admission Requirements</div>
+          <div>Program Details</div>
+          <div>Apply Now</div>
+        </div>
+      </section>
+      <ReceiveInfo />
+      <Footer />
+    </div>
+  );
 }
 
 export default Program;
